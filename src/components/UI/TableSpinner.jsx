4c3e3fe44bcf5c87096d9f0c35edcd8cfe773527
Patrick <patrick_btc_96@hotmail.com>
2024-07-10 20:09:54 +0200

    adjusted sizes of TableSpinner

--- src/components/UI/TableSpinner.jsx
+++ src/components/UI/TableSpinner.jsx
@@ -4,11 +4,11 @@ export const TableSpinner = () => {
   return (
     <Box overflow="hidden" mt={10}>
       <Spinner
-        thickness="4px"
+        thickness={{ base: 4, md: 6, "2xl": 8 }}
         speed="0.65s"
         emptyColor="#7D7D7D"
         color="#D4AF37"
-        size="xl"
+        boxSize={{ base: 16, md: 24, xl: 32 }}
       />
     </Box>
   );
